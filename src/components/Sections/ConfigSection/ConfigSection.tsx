1ca42a733291cf8bca1760b06c0762e63f7b3370
Andrew <baumgand000@stillwaterschools.org>
2025-03-12 09:40:26 -0500

    test2

--- src/components/Sections/ConfigSection/ConfigSection.tsx
+++ src/components/Sections/ConfigSection/ConfigSection.tsx
@@ -56,11 +56,7 @@ export function ConfigSection() {
           </SheetContent>
         </Sheet>
         <ThemeSelector />
-        <Button>
-
-              Hello
-
-        </Button>
+        <button type="button">Click Me!</button>
       </div>
     </Section>
   );
